1ac16ebd768bf931de14b7b0a3c389b7f79a49fd
Anuj Rathore <anujrathourstunts123@gmail.com>
2024-08-25 01:15:19 +0530

    Button.jsx file deleted

--- backgroundchanger/src/Button.jsx
+++
@@ -1,8 +0,0 @@
-// function Button ({color}){
-    
-//     return(
-//         <button onClick={setColor} className="px-4 py-2 rounded-full text-white text-xl font-semibold uppercase mx-2"style={{backgroundColor: color}}>{color}</button>
-//     )
-// }
-
-// export default Button;
